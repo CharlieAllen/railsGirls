d2c863ae636c5b2546e720a6ce68bc3190e3b673
CharlieAllen <miss.neophyte@gmail.com>
2015-12-05 14:06:22 +0000

    validations added

--- kiTindr/app/models/kitten.rb
+++ kiTindr/app/models/kitten.rb
@@ -1,3 +1,6 @@
 class Kitten < ActiveRecord::Base
-  
+  validates :kitten_api_id, uniqueness: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :kitten_api_id, presence: true
 end
